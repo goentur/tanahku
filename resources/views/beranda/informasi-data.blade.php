6c0196c5a15c39005fa4c098860a9830ab6336d5
Guntur <goentursumkid@gmail.com>
2025-12-09 13:53:12 +0700

    aaaa

--- resources/views/beranda/informasi-data.blade.php
+++ resources/views/beranda/informasi-data.blade.php
@@ -44,7 +44,7 @@
 	</div>
 	<div class="accordion-item">
 		<h2 class="accordion-header">
-			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#informasiPertanahan" aria-expanded="true" aria-controls="informasiPertanahan">
+			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#informasiPertanahan" aria-expanded="false" aria-controls="informasiPertanahan">
 				INFORMASI PERTANAHAN
 			</button>
 		</h2>
